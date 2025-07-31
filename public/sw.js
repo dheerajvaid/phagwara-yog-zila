5788087a23ce3d6706b168d7195d2ff70dd95166
Dheeraj Vaid <dheerajvaid@hotmail.com>
2025-07-31 20:25:46 +0530

    PWA added

---
+++ public/sw.js
@@ -0,0 +1,8 @@
+// public/sw.js
+self.addEventListener('install', event => {
+  console.log('✅ JoinYog SW installed');
+});
+
+self.addEventListener('fetch', event => {
+  // You can add cache logic later here
+});
