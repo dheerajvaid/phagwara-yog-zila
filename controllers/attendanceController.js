4670dd91fcbb6b49e32ed849f7bca77ac1e66bab
Dheeraj Vaid <dheerajvaid@hotmail.com>
2025-10-31 12:23:33 +0530

    attendance 0.65

--- controllers/attendanceController.js
+++ controllers/attendanceController.js
@@ -1100,7 +1100,7 @@ exports.viewTop10Attendance = async (req, res) => {
         selectedMonth === today.getMonth() + 1
           ? today.getDate()
           : end.getDate();
-      const threshold = Math.ceil(daysInMonth * 0.75);
+      const threshold = Math.ceil(daysInMonth * 0.65);
 
       Object.entries(kenderOperationalDaysMap).forEach(
         ([kenderId, datesSet]) => {
