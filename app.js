1538d29b41017b18229a3c9e37088f1b61e8573d
Dheeraj Vaid <dheerajvaid@hotmail.com>
2025-11-11 12:07:26 +0530

    healthz

--- app.js
+++ app.js
@@ -144,9 +144,15 @@ app.use("/paath", paathRoutes);
 app.use("/paath", paathGroupRoutes);
 
 
+// Health check route for Render
+app.get("/healthz", (req, res) => {
+  res.status(200).send("OK");
+});
+
 // Server Start
 const port = process.env.PORT || 3000;
+const host = "0.0.0.0";
 
-app.listen(port, '0.0.0.0', () => {
-  console.log(`✅ Server is running on http://0.0.0.0:${port}`);
+app.listen(port, host, () => {
+  console.log(`✅ Server is running on http://${host}:${port}`);
 });
