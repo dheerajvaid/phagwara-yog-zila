674f9b78d1c7b67163143b72321927128b686a9d
Dheeraj Vaid <dheerajvaid@hotmail.com>
2025-11-11 11:37:45 +0530

    server 0.0.0.0

--- app.js
+++ app.js
@@ -146,6 +146,7 @@ app.use("/paath", paathGroupRoutes);
 
 // Server Start
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`✅ Server is running on http://0.0.0.0:${port}`);
 });
